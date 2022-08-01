3c022095374f8ee75cffe637b0a07a561ffd0f18
Chris Wu <zezemint@gmail.com>
2022-08-01 13:46:37 -0700

    added space time complexity notes

--- Easy/LongestCommonPrefix/src/Solution.java
+++ Easy/LongestCommonPrefix/src/Solution.java
@@ -1,3 +1,12 @@
+/**
+ * Time complexity : O(S), where S is the sum of all characters in all strings.
+ *
+ * In the worst case all n strings are the same.
+ * There are S character comparisons, where S is the sum of all characters in the input array.
+ *
+ * Space complexity : O(1).
+ */
+
 public class Solution
 {
     public String longestCommonPrefix(String[] strs)
@@ -9,22 +18,30 @@ public class Solution
 
         String prefix = strs[0];
 
-        for(int i = 1; i < strs.length; i++)
-            while(strs[i].indexOf(prefix) != 0)
+        for(int i = 1; i < strs.length; i++) // iterate String[] starting @ the second string
+        {
+            System.out.println("looping!!!!!");
+
+            while(strs[i].indexOf(prefix) != 0)  // true if the prefix string does not start at index 0
             {
+                System.out.println("prefix: " + prefix);
+
                 prefix = prefix.substring(0, prefix.length() - 1);
+
                 if(prefix.isEmpty())
                 {
                     return "";
                 }
             }
+        }
         return prefix;
     }
 
     public static void main(final String[] args)
     {
-        String[] strArray = new String[]{"flower", "flower", "flower", "fl"};
+        String[] strArray = new String[]{"leets", "leetcode", "leet", "leeds"};
         Solution obj      = new Solution();
-        System.out.println(obj.longestCommonPrefix(strArray));
+        System.out.println("\n\n" + obj.longestCommonPrefix(strArray));
+
     }
 }
