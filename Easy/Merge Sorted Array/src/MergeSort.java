d222d6a92a656b20a613cde48644cba2f0554d79
Chris Wu <zezemint@gmail.com>
2022-08-11 14:25:33 -0700

    updated merge sort

--- Easy/Merge Sorted Array/src/MergeSort.java
+++ Easy/Merge Sorted Array/src/MergeSort.java
@@ -2,104 +2,74 @@
 
 class MergeSort
 {
-    // Divide the array into two subarrays, sort them and merge them
-    void mergeSort(final int[] arr,
-                   final int l,
-                   final int r)
+    public void mergeSort(int[] arr)
     {
-        if(l < r)
-        {
-            // m is the point where the array is divided into two subarrays
-            int m = (l + r) / 2;
-
-            // merge sort left subarray
-            mergeSort(arr, l, m);
-
-            // merge sort right subarray
-            mergeSort(arr, m + 1, r);
-
-            // Merge the sorted subarrays
-            merge(arr, l, m, r);
-        }
+        mergeSort(arr, new int[arr.length], 0, arr.length - 1);
     }
 
-    // Merge two subarrays L and M into arr
-    void merge(final int[] arr,
-               final int left,
-               final int middle,
-               final int right)
+    private void mergeSort(int[] arr,
+                           int[] temp,
+                           final int leftStart,
+                           final int rightEnd)
     {
-        int   leftArrSize;
-        int   rightArrSize;
-        int   i;
-        int   j;
-        int   k;
-        int[] leftSubArray;
-        int[] rightSubArray;
-
-        leftArrSize = middle - left + 1;
-        rightArrSize = right - middle;
-
-        leftSubArray = new int[leftArrSize];
-        rightSubArray = new int[rightArrSize];
-
-        populateSubArrays(arr, left, middle, leftArrSize, rightArrSize, leftSubArray, rightSubArray);
-
-        // Maintain current index of sub-arrays and main array
-        i = 0;
-        j = 0;
-        k = left;
-
-        // Place larger elements in the correct positions (till we reach either end of the subarrays)
-        while(i < leftArrSize && j < rightArrSize)
+        if(leftStart >= rightEnd)
         {
-            if(leftSubArray[i] <= rightSubArray[j])
+            return;
+        }
+
+        int m = (leftStart + rightEnd) / 2;
+
+        mergeSort(arr, temp, leftStart, m);
+
+        mergeSort(arr, temp, m + 1, rightEnd);
+
+        mergeHalves(arr, temp, leftStart, rightEnd);
+    }
+
+    private void mergeHalves(int[] arr,
+                             int[] temp,
+                             final int leftStart,
+                             final int rightEnd)
+    {
+        int leftEnd;
+        int rightStart;
+        int leftPointer;
+        int rightPointer;
+        int index;
+        int size;
+
+        leftEnd = (rightEnd + leftStart) / 2;
+        rightStart = leftEnd + 1;
+        leftPointer = leftStart;
+        rightPointer = rightStart;
+        index = leftStart;
+        size = rightEnd - leftStart + 1; // ***
+
+        while(leftPointer <= leftEnd && rightPointer <= rightEnd)
+        {
+            if(arr[leftPointer] <= arr[rightPointer])
             {
-                arr[k] = leftSubArray[i];
-                i++;
+                temp[index] = arr[leftPointer];
+                leftPointer++;
             }
             else
             {
-                arr[k] = rightSubArray[j];
-                j++;
+                temp[index] = arr[rightPointer];
+                rightPointer++;
             }
-            k++;
+            index++;
         }
 
-        // When we run out of elements in either L or M,
-        // pick up the remaining elements and put in A[p..r]
-        while(i < leftArrSize)
+        if(leftPointer > leftEnd)
         {
-            arr[k] = leftSubArray[i];
-            i++;
-            k++;
+            System.arraycopy(arr, rightPointer, temp, index, rightEnd - rightPointer + 1);
         }
 
-        while(j < rightArrSize)
+        if(rightPointer > rightEnd)
         {
-            arr[k] = rightSubArray[j];
-            j++;
-            k++;
+            System.arraycopy(arr, leftPointer, temp, index, leftEnd - leftPointer + 1);
         }
-    }
-
-    private void populateSubArrays(final int[] arr,
-                                   final int left,
-                                   final int middle,
-                                   final int leftArrSize,
-                                   final int rightArrSize,
-                                   final int[] leftSubArray,
-                                   final int[] rightSubArray)
-    {
-        for(int i = 0; i < leftArrSize; i++)
-        {
-            leftSubArray[i] = arr[left + i];
-        }
-
-        for(int j = 0; j < rightArrSize; j++)
-        {
-            rightSubArray[j] = arr[middle + 1 + j];
-        }
+        System.arraycopy(temp, leftStart, arr, leftStart, size);
     }
 
 
@@ -114,10 +84,10 @@ class MergeSort
     // Driver program
     public static void main(final String[] args)
     {
-        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int[] arr = {4, 3, 2, 1};
 
         MergeSort ob = new MergeSort();
-        ob.mergeSort(arr, 0, arr.length - 1);
+        ob.mergeSort(arr);
 
         System.out.println("Sorted array:");
         printArray(arr);
